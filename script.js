83e308688f6421719b09a8aedefe7e7aa3109219
unknown <esterknappverly@gmail.com>
2021-08-05 00:01:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("sou muito gostosa");
+var nome="Ester";
+var gostosa=true;
+var idade=20;
+var salario=20.071;
+console.log(nome,gostosa,idade,salario);
+function alerta01(){ 
+    alert("meu nome é Ester");
+}
